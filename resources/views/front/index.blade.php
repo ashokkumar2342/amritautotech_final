8862f318f12e7fb3c98e9034cff21308705365a7
ashokkumar2342 <ashokkumar2342@gmail.com>
2025-10-23 12:23:06 +0530

    update banner

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -15,6 +15,15 @@
                       </div>
                   </div>
               </li>
+              <li data-transition="fade">
+                  <img src="{{ asset('assets/images/slider/6.jpg') }}" alt="" width="1500" height="504"    data-bgrepeat="no-repeat" data-bgparallax="1">
+              </li>
+              <li data-transition="fade">
+                  <img src="{{ asset('assets/images/slider/7_1.jpg') }}" alt="" width="1500" height="504"    data-bgrepeat="no-repeat" data-bgparallax="1">
+              </li> 
+              <li data-transition="fade">
+                  <img src="{{ asset('assets/images/slider/8.jpg') }}" alt="" width="1500" height="504"    data-bgrepeat="no-repeat" data-bgparallax="1">
+              </li>
                <li data-transition="fade">
                   <img src="{{ asset('assets/images/slider/4.jpg') }}" alt="" width="1500" height="504"    data-bgrepeat="no-repeat">
                   <div class="tp-caption  tp-resizeme" data-x="center" data-hoffset="0" data-y="top" data-voffset="230" data-transform_idle="o:1;" data-transform_in="x:[-175%];y:0px;z:0;rX:0;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0.01;s:3000;e:Power3.easeOut;" data-transform_out="s:1000;e:Power3.easeInOut;s:1000;e:Power3.easeInOut;" data-mask_in="x:[100%];y:0;s:inherit;e:inherit;" data-splitin="none" data-splitout="none" data-start="700">
